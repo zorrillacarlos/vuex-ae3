19c5e46b8bc34d5de948de426afd6bc686445cad
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2022-05-26 10:53:19 -0400

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    mensaje:'Pasando mensaje por Vuex',
+    dia: 'Juernes',
+    cuadro:'Hola Mundo',
+    rectangulo:'estoy bien',
+    amigos:[],
+    amigo: null
   },
   getters: {
+    message (state){
+      return `${state.cuadro} ${state.rectangulo}`
+    }
   },
   mutations: {
+    addAmigo(state, params){
+      state.amigos = [...state.amigos, params]
+    }
   },
   actions: {
+    addAmigoAction ({commit}, params){
+      commit ('addAmigo', params)
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
+    <h2>{{$store.state.dia}}</h2>
+    <h1>LISTA DE AMIGOS INGRESADOS</h1>
+    <input type="text" v-model="amigoComponente">
+    <button @click="addAmigoComponente">AGREGAR AMIGO</button>
+
+    <ul v-for="(item, index) in amigos" :key="index">
+      <li>{{index + 1}} - {{item}}</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState } from 'vuex'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      amigoComponente:''
+    }
+  },
+  computed:{
+    ...mapState(['amigos'])
+  },
+
+  methods:{
+    addAmigoComponente(){
+      this.$store.dispatch('addAmigoAction', this.amigoComponente)
+    }
   }
 }
 </script>
